ebb4c2f541b4a389666c04a88ccec4c6f5b04e46
Alex Leigh <leigh@alexleigh.me>
2013-05-30 03:01:10 -0400

    Improved install script. Bump version to 1.99.07

--- installit.sh
+++ installit.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
+
 #
 # installit.sh - install a System/161 binary
 #
-# usage: installit.sh installdir prog version
+# usage: installit.sh installdir prog version [with-suffix]
 #
 
 INSTALLDIR="$1"
 PROG="$2"
 VERSION="$3"
 
-if [ "x$4" != x ]; then
-    echo "Usage: $0 installdir prog version" 1>&2
+if [ "x$4" = "xwith-suffix" ]; then
+    SUFFIX=1
+elif [ "x$4" = x ]; then
+    SUFFIX=0
+else
+    echo "Usage: $0 installdir prog version [with-suffix]" 1>&2
     exit 1
 fi
 
 ############################################################
 #
-# cross-check arguments
+# Cross-check arguments
 #
 
 if [ ! -d "$INSTALLDIR" ]; then
@@ -28,20 +33,20 @@ fi
 case "$PROG" in
     sys161|trace161|stat161|hub161) ;;
     *)
-	echo "$0: Invalid program $PROG" 1>&2
-	exit 1
-    ;;
+        echo "$0: Invalid program $PROG" 1>&2
+        exit 1
+        ;;
 esac
 
 case "$VERSION" in
-    devel) ;;				# devel version out of cvs
-    200[2-9][01][0-9][0-3][0-9]) ;;	# snap
-    [0-9].99.[0-9][0-9]) ;;		# devel release
-    [0-9].[0-9]|[0-9].[0-9][0-9]) ;;	# release
+    devel) ;; # devel version out of cvs
+    200[2-9][01][0-9][0-3][0-9]) ;; # snap
+    [0-9].99.[0-9][0-9]) ;; # devel release
+    [0-9].[0-9]|[0-9].[0-9][0-9]) ;; # release
     *)
-	echo "$0: Invalid version $VERSION" 1>&2
-	exit 1
-    ;;
+        echo "$0: Invalid version $VERSION" 1>&2
+        exit 1
+        ;;
 esac
 
 ############################################################
@@ -49,44 +54,38 @@ esac
 # Do it.
 #
 
-VTARG="${INSTALLDIR}/${PROG}-${VERSION}"
-TARG="${INSTALLDIR}/${PROG}"
+if [ $SUFFIX = 1 ]; then
+    PROGPATH="${INSTALLDIR}/${PROG}-${VERSION}"
+else
+    PROGPATH="${INSTALLDIR}/${PROG}"
+fi
 
 #
 # Copy the file.
 #
-cp "${PROG}" "${VTARG}.new" || exit 1
-chmod 755 "${VTARG}.new"
+cp "${PROG}" "${PROGPATH}.new" || exit 1
+chmod 755 "${PROGPATH}.new"
 
 #
 # We might be installing the same version a second time, particularly
 # if the version is "devel". Move the old one out of the way to avoid
 # ETXTBUSY on some systems if it's running. But suppress any errors.
 #
-mv -f "${VTARG}" "${VTARG}.old" >/dev/null 2>&1
+mv -f "${PROGPATH}" "${PROGPATH}.old" >/dev/null 2>&1
 
 #
 # Put the new one in place.
 #
-mv -f "${VTARG}.new" "${VTARG}" || exit 1
+mv -f "${PROGPATH}.new" "${PROGPATH}" || exit 1
 
 #
-# Now update the symbolic links.
-# We want sys161.old to point to the previous installed *version*, even if
-# we reinstall the same version several times. So do some silly checks.
+# Remove the old one.
 #
+rm -f "${PROGPATH}.old" >/dev/null 2>&1
 
-SKIP=0
-if [ -h "${TARG}" -a -h "${TARG}.old" ]; then
-    PREVLINK=`ls -l ${TARG} | awk '{ print $NF }'`
-    if [ "x$PREVLINK" = "x${PROG}-${VERSION}" ]; then
-	SKIP=1
-    fi
+#
+# Update the symbolic links
+#
+if [ $SUFFIX = 1 ]; then
+    ln -sf "${PROG}-${VERSION}" "${INSTALLDIR}/${PROG}" || exit 1
 fi
-
-if [ $SKIP = 0 ]; then
-    rm -f "${TARG}.old" >/dev/null 2>&1
-    mv -f "${TARG}" "${TARG}.old" >/dev/null 2>&1
-    ln -s "${PROG}-${VERSION}" "${TARG}" || exit 1
-fi
-
--- makerules.sh
+++ makerules.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 #
 # makerules - write makefile rules for System/161 build
 #
@@ -9,38 +10,38 @@
 
 
 awk '{ for (i=1; i<=NF; i++) print $i; }' |\
-  awk '
-    BEGIN {
+awk '
+BEGIN {
 	curdir = "NOGOOD";
 	printf "# Automatically generated file - do not edit\n\n";
-    }
+}
 
-    function rule(file, base) {
+function rule(file, base) {
 	printf "%s.o: %s/%s\n", base, curdir, file;
 	printf "\t$(CC) $(CFLAGS) -I%s -c %s/%s\n", curdir, curdir, file;
 	printf "SRCS+=%s/%s\n", curdir, file;
 	printf "OBJS+=%s.o\n", base;
 	printf "\n";
-    }
+}
 
-    /\.[cCsS]$/ {
+/\.[cCsS]$/ {
 	file = $1;
 	base = $1;
 	sub("\\.[cCsS]$", "", base);
 	rule(file, base);
 	next;
-    }
+}
 
-    /\.cc$/ {
+/\.cc$/ {
 	file = $1;
 	base = $1;
 	sub("\\.cc$", "", base);
 	rule(file, base);
 	next;
-    }
+}
 
-    {
+{
 	curdir = "$S/" $1;
 	printf "DEPINCLUDES+=-I%s\n\n", curdir;
-    }
+}
 '
